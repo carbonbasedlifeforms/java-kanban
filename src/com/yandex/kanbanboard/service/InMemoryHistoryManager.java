1233b215ddc9157eb5f94be0aa3a40f6a3d13a13
Ravil Bikkulov <r4vilbikkulov@yandex.ru>
2024-08-04 22:45:44 +0300

    move class Node to separate class

--- src/com/yandex/kanbanboard/service/InMemoryHistoryManager.java
+++ src/com/yandex/kanbanboard/service/InMemoryHistoryManager.java
@@ -73,41 +73,41 @@ public class InMemoryHistoryManager implements HistoryManager {
     public List<Task> getHistory() {
         return getTasks();
     }
+}
 
-    private static class Node<T> {
-        private T value;
-        private Node<T> prev;
-        private Node<T> next;
+class Node<Task> {
+    private Task value;
+    private Node<Task> prev;
+    private Node<Task> next;
 
-        public Node(T value, Node<T> prev, Node<T> next) {
-            this.value = value;
-            this.prev = prev;
-            this.next = next;
-        }
+    public Node(Task value, Node<Task> prev, Node<Task> next) {
+        this.value = value;
+        this.prev = prev;
+        this.next = next;
+    }
 
-        public T getValue() {
-            return value;
-        }
+    public Task getValue() {
+        return value;
+    }
 
-        public Node<T> getPrev() {
-            return prev;
-        }
+    public Node<Task> getPrev() {
+        return prev;
+    }
 
-        public Node<T> getNext() {
-            return next;
-        }
+    public Node<Task> getNext() {
+        return next;
+    }
 
-        public void setNext(Node<T> next) {
-            this.next = next;
-        }
+    public void setNext(Node<Task> next) {
+        this.next = next;
+    }
 
-        public void setValue(T value) {
-            this.value = value;
-        }
+    public void setValue(Task value) {
+        this.value = value;
+    }
 
-        public void setPrev(Node<T> prev) {
-            this.prev = prev;
-        }
+    public void setPrev(Node<Task> prev) {
+        this.prev = prev;
     }
 }
 
